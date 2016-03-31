efd3f66939d8502d1b68e5cd626c1682cdb88b5c
tsmanner <zephrincochrane@gmail.com>
2016-03-31 15:23:49 -0400

    First commit. Seems to work, but hasn't been tested against very many XML files.

---
+++ pyxml.py
@@ -0,0 +1,167 @@
+import os
+import re
+import string
+
+
+class XmlSyntaxError(SyntaxError):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class XmlParser:
+    def __init__(self, xml_file):
+        self.xml_file = xml_file
+        self.stream = None
+        self.charno = 0
+        self.lineno = 1
+        self.token = ""
+        self.data = {}
+        self.parse()
+
+    def next_char(self):
+        self.char = self.stream.read(1)
+        if self.char == os.linesep:
+            self.lineno += 1
+            self.charno = 0
+        self.charno += 1
+
+    def parse(self):
+        with open(self.xml_file) as self.stream:
+            self.lineno = 1
+            self.charno = 0
+            self.token = ""
+            self.next_char()
+            while self.char != "":
+                if self.char in string.whitespace:
+                    self.next_char()
+                    continue
+                if self.char != "<" and self.token == "":
+                    msg = 'Text encountered outside tags in "' + self.xml_file +\
+                          '", line ' + str(self.lineno) + ', char ' + str(self.charno)
+#                    raise XmlSyntaxError(msg)
+                    self.next_char()
+                else:
+                    self.token += self.char
+                    while self.char not in string.whitespace + ">":
+                        self.next_char()
+                        self.token += self.char
+                    if self.token.startswith("<!--"):
+#                        print("comment1")
+                        self.parse_comment()
+                    else:
+                        tag = self.parse_tag()
+                    self.token = ""
+                self.data[tag.name] = tag
+
+    def parse_comment(self):
+        while not self.token.endswith("-->"):
+            self.next_char()
+            self.token += self.char
+#        print(self.token)
+        self.token = ""
+        self.next_char()
+
+    def parse_tag(self):
+        tagname = self.token.strip(string.whitespace + "<>")
+#        print("token:", self.token, "name:", tagname)
+        tag = XmlTag(tagname)
+        self.next_char()
+        if not self.token.endswith(">"):
+            self.parse_attributes(tag)
+        self.parse_elements(tag)
+        return tag
+
+    def parse_attributes(self, tag):
+        self.token = ""
+        while True:
+            while self.char in string.whitespace:
+                self.next_char()
+            while self.char not in string.whitespace + ">":
+                self.token += self.char
+                self.next_char()
+#            print("token:", self.token, "char:", self.char)
+            if self.token.startswith("<!--"):
+#                print("comment2")
+                self.parse_comment()
+            else:
+                tag.add_attribute(self.token)
+            if self.char == ">":
+                # We're done parsing attributes, move on to the contents
+                break
+            elif self.char == "":
+                msg = 'EoF encountered while parsing tag in "' + self.xml_file +\
+                      '", line ' + str(self.lineno) + ', char ' + str(self.charno)
+                raise XmlSyntaxError(msg)
+            self.token = ""
+        # Advance one char past the ">"
+        self.next_char()
+
+    def parse_elements(self, tag):
+        self.token = ""
+        while True:
+            if self.token.startswith("<!--"):
+#                print("comment3")
+                self.parse_comment()
+            if self.char in string.whitespace:
+                self.next_char()
+                continue
+            self.token += self.char
+            while True:
+                self.next_char()
+                if self.char in string.whitespace + "<":
+                    break
+                self.token += self.char
+#            print("token:", self.token, "char:", self.char)
+            if self.token.startswith("<!--"):
+                self.parse_comment()
+            elif self.token.startswith("</" + tag.name):
+                self.next_char()
+#                print("closing tag", tag.name)
+                break
+            elif self.token.startswith("<"):
+                if len(self.token) == 1:
+                    self.next_char()
+                    continue
+                tag.elements.append(self.parse_tag())
+            else:
+                tag.elements.append(self.token)
+            self.token = ""
+
+
+class XmlTag:
+    def __init__(self, name):
+        self.name = name
+        self.attributes = {}
+        self.elements = []
+
+    def add_attribute(self, token):
+        split = token.split("=")
+        if re.match("[0-9]+", split[1]):
+            self.attributes[split[0]] = int(split[1])
+        elif re.match("[0-9A-Fa-f]+", split[1]):
+            self.attributes[split[0]] = int(split[1], 16)
+        elif re.match("[0-9]+\.[0-9]+", split[1]):
+            self.attributes[split[0]] = float(split[1])
+        elif re.match("[0-9A-Fa-f]+\.[0-9A-Fa-f]+", split[1]):
+            self.attributes[split[0]] = float(split[1])
+        else:
+            self.attributes[split[0]] = split[1]
+
+    def __str__(self):
+        s = "<" + self.name
+        for key in self.attributes:
+            s += " " + key + "=" + self.attributes[key]
+        s += ">"
+        s += ' '.join([str(element) for element in self.elements])
+        s += "</" + self.name + ">"
+        return s
+
+
+def parse_xml(filename):
+    return XmlParser(filename).data
+
+if __name__ == "__main__":
+    xml_file = ".devdirs"
+    parser = XmlParser(xml_file)
+    [print(parser.data[k]) for k in parser.data]
+
